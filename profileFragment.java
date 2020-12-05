95e1d2004d829261523052fc5aece69ec1e8a172
emonnelson <39386877+emonnelson@users.noreply.github.com>
2020-12-04 22:07:20 -0500

    Create profileFragment.java

---
+++ profileFragment.java
@@ -0,0 +1,46 @@
+package fragments;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.r.myapplication.Post;
+
+import java.util.List;
+
+public class ProfileFragment extends PostsFragment {
+
+    @Override
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.USER_KEY);
+        query.whereEqualTo(Post.USER_KEY, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+
+
+
+
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+    }
+
+}
